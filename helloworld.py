dc7e1311fd29246884045b9f61ec1346fd58f088
Eddy Joseph <joeddy1@gmail.com>
2016-05-07 12:44:34 -0400

    Learning Python 4th Ed study codes

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 def main():
     print('Hello World!')
     
     
-if __name__ == '__main__': main()
+    
+if __name__ == '__main__':
+    main()
